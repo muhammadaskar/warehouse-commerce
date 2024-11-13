4fdefe68ad7df25783ca638860a7dbd163f88200
Muhammad Askar <mhmmdaskar11@gmail.com>
2024-11-14 02:10:48 +0700

    refactor version

--- warehouse-service/warehouse-domain/warehouse-application-service/src/main/java/com/ecommerce/app/warehouse/domain/service/dto/create/CreateWarehouseCommand.java
+++ warehouse-service/warehouse-domain/warehouse-application-service/src/main/java/com/ecommerce/app/warehouse/domain/service/dto/create/CreateWarehouseCommand.java
@@ -1,28 +1,23 @@
 package com.ecommerce.app.warehouse.domain.service.dto.create;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Builder
 @AllArgsConstructor
-@JsonDeserialize(builder = CreateWarehouseCommand.CreateWarehouseCommandBuilder.class)
+@NoArgsConstructor(force = true)
 public class CreateWarehouseCommand {
     @NotNull
-    @JsonProperty("name")
     private final String name;
     @NotNull
-    @JsonProperty("street")
     private final String street;
     @NotNull
-    @JsonProperty("postalCode")
     private final String postalCode;
     @NotNull
-    @JsonProperty("city")
     private final String city;
 }
--- warehouse-service/warehouse-messaging/src/main/java/com/ecommerce/app/warehouse/messaging/mapper/WarehouseMessagingDataMapper.java
+++ warehouse-service/warehouse-messaging/src/main/java/com/ecommerce/app/warehouse/messaging/mapper/WarehouseMessagingDataMapper.java
@@ -5,8 +5,6 @@ import com.ecommerce.app.warehouse.domain.core.entity.Warehouse;
 import com.ecommerce.app.warehouse.domain.core.event.WarehouseCreatedEvent;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 public class WarehouseMessagingDataMapper {
 
