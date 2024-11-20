9333e394c093c6375a642de14389c47633500a48
Muhammad Askar <mhmmdaskar11@gmail.com>
2024-11-20 22:06:57 +0700

    change create topic

--- infrastructure/kubernetes/create-topics.sh
+++ infrastructure/kubernetes/create-topics.sh
@@ -1,6 +1,10 @@
-#local-confluent-kafka-cp-zookeeper-headless
-kafka-topics --zookeeper $1:2181 --topic warehouse-create-topic --delete --if-exists
-kafka-topics --zookeeper $1:2181 --topic warehouse-admin-create-topic --delete --if-exists
+kubectl exec -it kafka-client sh
 
-kafka-topics --zookeeper $1:2181 --topic warehouse-create-topic --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper $1:2181 --topic warehouse-admin-create-topic --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --list
+
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic warehouse-create-topic --delete --if-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic warehouse-admin-create-topic --delete --if-exists
+
+
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic warehouse-create-topic --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic warehouse-admin-create-topic --create --partitions 3 --replication-factor 3 --if-not-exists
